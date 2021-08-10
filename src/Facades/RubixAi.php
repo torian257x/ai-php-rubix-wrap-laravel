fce0f1d432f28ee2263b53d55f01e1d1f32b1618
jossnaz <lukas.meier@gmail.com>
2021-08-10 18:39:33 -0500

    Adding phpdoc to facade

--- src/Facades/RubixAi.php
+++ src/Facades/RubixAi.php
@@ -4,6 +4,16 @@ namespace Torian257x\RubixAi\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ *
+ * @method static array getConfig(string $config_entry = null)
+ * @method static array train($data, mixed $data_index_w_label = null, \Rubix\ML\Estimator $estimator_algorithm = null, array $transformers = null, $model_filename = 'model_trained.rbx', float $train_part_size = 0.7)
+ * @method static array trainWithoutTest($data, mixed $data_index_w_label = null, \Rubix\ML\Estimator $estimator_algorithm = null, array $transformers = null, $model_filename = 'model_trained.rbx')
+ * @method static array|int predict($input_data, \Rubix\ML\Estimator $estimator = null, string $model_filename = 'model_trained.rbx')
+ * @method static array getErrorAnalysis($samples_w_labels, $key_for_labels, $model_filename = 'model_trained.rbx')
+ *
+ * @see \Torian257x\RubixAi\RubixAiService
+ */
 class RubixAi extends Facade
 {
     /**
