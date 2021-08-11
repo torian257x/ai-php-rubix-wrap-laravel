a7e935fcee4afb46a96de4b7764faccd5c1b4c21
jossnaz <lukas.meier@gmail.com>
2021-08-10 21:40:25 -0500

    rubixai_config.php fixing path from double /

--- config/rubixai_config.php
+++ config/rubixai_config.php
@@ -3,8 +3,8 @@
 use Torian257x\RubWrap\Service\RubixService;
 
 return [
-    'csv_path_output' => storage_path('/ai_rubix/csv/'),
-    'csv_path_input' => storage_path('/ai_rubix/csv/'),
-    'ai_model_path_output' => storage_path('/ai_rubix/model/'),
+    'csv_path_output' => storage_path('ai_rubix/csv/'),
+    'csv_path_input' => storage_path('ai_rubix/csv/'),
+    'ai_model_path_output' => storage_path('ai_rubix/model/'),
     'RubixMainClass' => RubixService::class,
 ];
